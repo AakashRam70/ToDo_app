d952883842e5327d47187fdcc8c13810c2307693
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-17 01:51:39 +0530

    Adding types.js and zod

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,34 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const PORT = 3200;
 const app = express();
 
 app.use(express.json());
 
-app.post("/todos", function(req,res){
+app.post("/todos", function (req, res) {
+    const createPayload = req.body;
+    const parsedPayload = updateTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You send a wrong inputs",
+        })
+        return;
+    }
+})
+
+app.get("/todos", function (req, res) {
 
 })
 
-app.get("/todos", function(req,res){
-
-})
-
-app.post("/completed", function(req,res){
-
+app.post("/completed", function (req, res) {
+    const upadatePayload = req.body;
+    const safePayload = createTodo.safeParse(upadatePayload);
+    if (!safePayload.success) {
+        req.status(411).json({
+            msg: "Not a valid Inputs"
+        })
+        return;
+    }
 })
 
 app.listen(function () {
